7247d8fb483cb27992d1f387bb5c0f02d17026da
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-18 18:57:24 +0600

    last commit , not working with this repo lter

--- components/Header.js
+++ components/Header.js
@@ -7,18 +7,18 @@ const Header = () => {
     <div>
       <div className="flex justify-between max-w-6xl">
         {/* Left */}
-        <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
           <Image src={longLogo} layout="fill" objectFit="contain"></Image>
         </div>
-        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
           <Image src={shortLogo} layout="fill" objectFit="contain"></Image>
         </div>
         {/* Middle - search input field */}
-        <div className="relative mt-1 p-3 rounded bg-red-500">
+        <div className="relative mt-1 p-3 rounded-md">
           <div className="absolute inset-y-0 pl-3 flex item-center pointer-events-none">
             <SearchIcon className="h-5 w-5 text-gray-500"/>
           </div>
-          <input type="text" placeholder="Search" />
+          <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-red-300 rounded-md" type="text" placeholder="Search" />
         </div>
         {/* Right */}
       </div>
